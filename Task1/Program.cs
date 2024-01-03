fa6444f3fc67cf2b9adfec2deff26be800a34569
SergeyAnopreenko <154093984+SergeyAnopreenko@users.noreply.github.com>
2024-01-03 21:04:22 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Text;
+
+Console.WriteLine("Enter symbols");
+
+var UserInput = Console.ReadLine();
+
+StringBuilder Array = new StringBuilder(UserInput);
+
+for (int i = 0; i < Array.Length; i++)
+{
+    char Simbol = Array[i];
+
+    int Counter = 1;
+
+    for (int j = i + 1; j < Array.Length; j++)
+    {
+        if (Simbol == Array[j])
+        {
+            Counter++;
+
+            Array.Remove(j, 1);
+
+            j--;
+        }
+    }
+    Console.WriteLine($" {Simbol} - {Counter} ");
+}
+
+
